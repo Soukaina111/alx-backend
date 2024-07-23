4b6bcba43bc62dca786fd6fa6e31cf04f0eb989b
Soukaina111 <basmahajar@gmail.com>
2024-07-23 15:25:31 +0000

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,6 +7,7 @@ from threading import RLock
 # Import the BaseCaching class from the 'base_caching' module
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class FIFOCache(BaseCaching):
     """
     An implementation of FIFO (First In First Out) Cache
@@ -15,6 +16,7 @@ class FIFOCache(BaseCaching):
         __keys (list): Stores cache keys in order of entry using `.append`
         __rlock (RLock): Lock accessed resources to prevent race condition
     """
+
     def __init__(self):
         """
         Instantiation method, sets instance attributes.
@@ -51,11 +53,13 @@ class FIFOCache(BaseCaching):
             key (Any): The key of the item to be retrieved.
 
         Returns:
-            The value associated with the given key, or None if the key is not found.
+            The value associated with the given key
+            or None if the key is not found.
         """
         # Acquire the lock to ensure thread-safe access to the cache
         with self.__rlock:
-            # Retrieve the value associated with the given key from the cache_data dictionary
+            # Retrieve the value associated with the given key from the
+            # cache_data dictionary
             return self.cache_data.get(key, None)
 
     def _balance(self, keyIn):
@@ -78,7 +82,8 @@ class FIFOCache(BaseCaching):
                 # If the cache is at maximum capacity, evict the oldest item
                 if len(self.cache_data) == BaseCaching.MAX_ITEMS:
                     OutK = self.__keys.pop(0)  # Remove the oldest key
-                    self.cache_data.pop(OutK)  # Remove the corresponding item from the cache
+                    # Remove the corresponding item from the cache
+                    self.cache_data.pop(OutK)
                 # Add the new key to the end of the __keys list
                 self.__keys.insert(LengthofK, keyIn)
         return OutK
