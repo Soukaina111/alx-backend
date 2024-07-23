b7c5233a9fc519ae9e755baf7640113a021ab055
Soukaina111 <basmahajar@gmail.com>
2024-07-23 17:18:32 +0000

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,68 +1,85 @@
 #!/usr/bin/python3
 """MRU Cache Replacement Implementation Class
 """
+# Importing RLock from threading module for thread-safe operations
 from threading import RLock
 
-BaseCaching = import('base_caching').BaseCaching
+# Dynamically importing BaseCaching class
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-
     """
-An implementation of MRU(Most Recently Used) Cache
-Attributes:
-    __keys (list): Stores cache keys from least to most accessed
-    __rlock (RLock): Lock accessed resources to prevent race condition
-"""
+    An implementation of MRU(Most Recently Used) Cache
 
-
-def __init__(self):
-    """ Instantiation method, sets instance attributes
+    Attributes:
+        __keys (list): Stores cache keys from least to most accessed
+        __rlock (RLock): Lock accessed resources to prevent race condition
     """
-    super().__init__()
-    self.__keys = []  # Initialize the list to store cache keys
-    self.__rlock = RLock()  # Create a reentrant lock for thread safety
 
+    def __init__(self):
+        """ Instantiation method, sets instance attributes
+        """
+        super().__init__()
+        self.__keys = []
+        self.__rlock = RLock()
 
-def put(self, key, item):
-    """ Add an item in the cache
-    """
-    if key is not None and item is not None:
-        # Call the _balance method to update the cache
-        keyO = self._balance(key)
-        with self.__rlock:
-            # Add the new item to the cache
-            self.cache_data.update({key: item})
-        if keyO is not None:
-            # Print the discarded key, if any
-            print('DISCARD: {}'.format(keyO))
+    def put(self, key, item):
+        """ Add an item in the cache
 
+        Args:
+            key: The unique identifier for the item to be cached.
+            item: The actual content to be cached.
 
-def get(self, key):
-    """ Get an item by key
-    """
-    with self.__rlock:
-        # Retrieve the item from the cache
-        value = self.cache_data.get(key, None)
-        if key in self.__keys:
-            self._balance(key)  # Call the _balance method to update the cache
-    return value
+        """
+        if key is not None and item is not None:
+            # Check and possibly remove the least recently used item
+            OutK = self._balance(key)
+            with self.__rlock:  # Ensure thread safety
+                # Update the cache with the new item
+                self.cache_data.update({key: item})
+            if OutK is not None:
+                # Inform about discarded item if necessary
+                print('DISCARD: {}'.format(OutK))
 
+    def get(self, key):
+        """ Get an item by key
 
-def _balance(self, keyIn):
-    """ Removes the earliest item from the cache at MAX size
-    """
-    keyO = None  # Initialize the variable to store the discarded key
-    with self.__rlock:
-        LenghtOFK = len(self.__keys)  # Get the length of the __keys list
-        if keyIn not in self.__keys:
-            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                # Remove the least recently used key
-                keyO = self.__keys.pop(LenghtOFK - 1)
-                # Remove the corresponding item from the cache
-                self.cache_data.pop(keyO)
-        else:
-            self.__keys.remove(keyIn)  # Remove the existing key from the list
-        # Add the key to the end of the list
-        self.__keys.insert(LenghtOFK, keyIn)
-    return keyO
+        Args:
+            key: The unique identifier for the item to retrieve from the cache.
+
+        Returns:
+        Updates the access order of the retrieved item.
+        """
+        with self.__rlock:  # Ensure thread safety
+            # Retrieve the item from the cache
+            value = self.cache_data.get(key, None)
+            if key in self.__keys:
+                # Update the access order of the retrieved item
+                self._balance(key)
+        return value
+
+    def _balance(self, keyIn):
+        """ Removes the earliest item from the cache at MAX size
+
+        Args:
+            keyIn: The key of the item whose access order needs to be updated.
+
+        Returns:
+            The key of the discarded item, if any, otherwise None.
+
+        Also updates the access order of the specified key.
+        """
+        OutK = None
+        with self.__rlock:  # Ensure thread safety
+            # Current number of keys in the cache
+            keysLength = len(self.__keys)
+            if keyIn not in self.__keys:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    # Remove the least recently used item
+                    OutK = self.__keys.pop(keysLength - 1)
+                    self.cache_data.pop(OutK)  # Remove the item from the cache
+            else:
+                self.__keys.remove(keyIn)
+            self.__keys.insert(keysLength, keyIn)
+        return OutK
