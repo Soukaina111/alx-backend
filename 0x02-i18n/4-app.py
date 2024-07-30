a80920079f310d57bb0c0a4ff9a4d97882b33520
Soukaina111 <basmahajar@gmail.com>
2024-07-30 19:01:35 +0000

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+
+# Import the Flask class from the flask module
+from flask import Flask
+# Import the request object from the flask module
+from flask import request
+# Import the render_template function from the flask module
+from flask import render_template
+# Import the Babel class from the flask_babel module
+from flask_babel import Babel
+
+# Define a configuration class for the application
+
+
+class Config(object):
+    """
+    Application configuration class
+    """
+    # Define the supported languages
+    LANGUAGES = ['en', 'fr']
+    # Set the default locale and timezone
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+# Instantiate the Flask application object
+
+
+app = Flask(__name__)
+# Load the configuration from the Config class
+
+
+app.config.from_object(Config)
+
+# Wrap the Flask application with the Babel extension
+babel = Babel(app)
+
+# Define a function to select the locale based on the request object
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    # Get the 'locale' query parameter from the request
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# Define a route for the root URL '/'
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    # Use the render_template function to render the '4-index.html' template
+    return render_template('4-index.html')
+
+# Check if the script is being run directly (not imported as a module)
+
+
+if __name__ == '__main__':
+    # Run the Flask application
+    app.run()
