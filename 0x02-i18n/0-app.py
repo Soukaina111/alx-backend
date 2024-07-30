753651f277ea91f33a31af461a1edffb04b860ab
Soukaina111 <basmahajar@gmail.com>
2024-07-30 18:25:00 +0000

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+
+# Import the Flask class from the flask module
+from flask import Flask
+# Import the render_template function from the flask module
+from flask import render_template
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route for the root URL '/'
+# The strict_slashes=False parameter ensures that URLs with and without a trailing slash are both handled
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    # Use the render_template function to render the '0-index.html' template
+    return render_template('0-index.html')
+
+# Check if the script is being run directly (not imported as a module)
+if __name__ == '__main__':
+    # Run the Flask application
+    app.run()
